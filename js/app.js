d63adc8c4745d1fe72783c132c9505bdb5970491
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-12-30 13:16:02 +0000

    alpha version created

--- js/app.js
+++ js/app.js
@@ -1,67 +1,76 @@
 const songs = [
-    {name: "Колыбельная", author: "Jah khalib", src: './audios/audio-1.mp3'},
-    {name: "Let Me Down Slowly", author: "Alec Bejamin", src: './audios/audio-2.mp3'},
-    {name: "Берега", author: "Timran & Zell & Batrai", src: './audios/audio-3.mp3'},
-    {name: "Фея", author: "SHAMI", src: './audios/audio-4.mp3'},
-]
-const audioTag = document.querySelector('#audio')
-const next = document.querySelector('#next')
-const pause = document.querySelector('#pause')
-const prev = document.querySelector('#prev')
-const range = document.querySelector('#range')
-const img = document.querySelector('#img')
-const title = document.querySelector('#title')
-let index = 2
-audioTag.src = songs[index].src
-title.textContent = `${songs[index].name} - ${songs[index].author}`
-function playMusic(){
-    if(audioTag.paused){
-        audioTag.play()
-        img.classList.add('playing')
-        pause.classList.add('fa-circle-pause')
-        pause.classList.remove('fa-circle-play')
-    }
-    else{
-        audioTag.pause()
-        img.classList.remove('playing')
-        pause.classList.remove('fa-circle-pause')
-        pause.classList.add('fa-circle-play')
-    }
+  { name: "Колыбельная", author: "Jah khalib", src: "./audios/audio-1.mp3" },
+  {
+    name: "Let Me Down Slowly",
+    author: "Alec Bejamin",
+    src: "./audios/audio-2.mp3",
+  },
+  {
+    name: "Берега",
+    author: "Timran & Zell & Batrai",
+    src: "./audios/audio-3.mp3",
+  },
+  { name: "Фея", author: "SHAMI", src: "./audios/audio-4.mp3" },
+];
+const audioTag = document.querySelector("#audio");
+const next = document.querySelector("#next");
+const pause = document.querySelector("#pause");
+const prev = document.querySelector("#prev");
+const range = document.querySelector("#range");
+const img = document.querySelector("#img");
+const title = document.querySelector("#title");
+let index = localStorage.index || 0;
+audioTag.src = songs[index].src;
+title.textContent = `${songs[index].name} - ${songs[index].author}`;
+function playMusic() {
+  localStorage.setItem("index", index);
+  document.title = `${songs[index].name} - ${songs[index].author}`;
+  if (audioTag.paused) {
+    audioTag.play();
+    img.classList.add("playing");
+    pause.classList.add("fa-pause");
+    pause.classList.remove("fa-play");
     setInterval(() => {
-        const percent = audioTag.duration
-        console.log(percent / audioTag.currentTime)
-    }, 1000)
-   
+      const percent = (audioTag.currentTime / audioTag.duration) * 100;
+      range.value = percent;
+      if (audioTag.ended) {
+        nextMusic();
+      }
+    }, 500);
+  } else {
+    audioTag.pause();
+    img.classList.remove("playing");
+    pause.classList.remove("fa-pause");
+    pause.classList.add("fa-play");
+  }
 }
-function changeEvent(){
-    const percent = audioTag.duration / 100
-    audioTag.currentTime = range.value * percent
+function changeEvent() {
+  const percent = audioTag.duration / 100;
+  audioTag.currentTime = range.value * percent;
 }
 
-function nextMusic(){
-    range.value = 0
-    if(index == songs.length - 1){
-        index = 0
-    }
-    else{
-        index++
-    }
-    title.textContent = `${songs[index].name} - ${songs[index].author}`
-    audioTag.src = songs[index].src
-    playMusic()
+function nextMusic() {
+  range.value = 0;
+  if (index == songs.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  title.textContent = `${songs[index].name} - ${songs[index].author}`;
+  audioTag.src = songs[index].src;
+  playMusic();
 }
-function prevMusic(){
-    range.value = 0
-     if(index == 0){
-        index = songs.length - 1
-    }
-    else{
-        index--
-    }
-    title.textContent = `${songs[index].name} - ${songs[index].author}`
-    audioTag.src = songs[index].src
-    playMusic()
+function prevMusic() {
+  range.value = 0;
+  if (index == 0) {
+    index = songs.length - 1;
+  } else {
+    index--;
+  }
+  title.textContent = `${songs[index].name} - ${songs[index].author}`;
+  audioTag.src = songs[index].src;
+  playMusic();
 }
-next.addEventListener('click', nextMusic)
-prev.addEventListener('click', prevMusic)
-pause.addEventListener('click', playMusic)
+next.addEventListener("click", nextMusic);
+prev.addEventListener("click", prevMusic);
+pause.addEventListener("click", playMusic);
