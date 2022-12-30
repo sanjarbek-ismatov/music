4489af756a4cf0934eee7b8d46da3e5bdce97c2a
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-12-30 14:58:09 +0000

    beta version released

--- js/app.js
+++ js/app.js
@@ -19,9 +19,22 @@ const prev = document.querySelector("#prev");
 const range = document.querySelector("#range");
 const img = document.querySelector("#img");
 const title = document.querySelector("#title");
+const currentTime = document.querySelector("#currentTime");
+const fullTime = document.querySelector("#fullTime");
+const volume = document.querySelector("#volume");
+const volumePercent = document.querySelector("#volumePercent");
 let index = localStorage.index || 0;
 audioTag.src = songs[index].src;
 title.textContent = `${songs[index].name} - ${songs[index].author}`;
+
+
+audioTag.onloadedmetadata = () => {
+  const toMinute = Math.floor(audioTag.duration / 60);
+  const toSecond = Math.floor(audioTag.duration - toMinute * 60);
+  fullTime.textContent = `${toMinute}:${
+    toSecond > 9 ? toSecond : "0" + toSecond
+  }`;
+};
 function playMusic() {
   localStorage.setItem("index", index);
   document.title = `${songs[index].name} - ${songs[index].author}`;
@@ -33,6 +46,11 @@ function playMusic() {
     setInterval(() => {
       const percent = (audioTag.currentTime / audioTag.duration) * 100;
       range.value = percent;
+      const toMinute = Math.floor(audioTag.currentTime / 60);
+      const toSecond = Math.floor(audioTag.currentTime - toMinute * 60);
+      currentTime.textContent = `${toMinute}:${
+        toSecond > 9 ? toSecond : "0" + toSecond
+      }`;
       if (audioTag.ended) {
         nextMusic();
       }
@@ -48,7 +66,10 @@ function changeEvent() {
   const percent = audioTag.duration / 100;
   audioTag.currentTime = range.value * percent;
 }
-
+function changeVolume() {
+  audioTag.volume = +volume.value / 100;
+  volumePercent.textContent = `Ovoz: ${volume.value}%`;
+}
 function nextMusic() {
   range.value = 0;
   if (index == songs.length - 1) {
